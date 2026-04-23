b54874bd2352b2c60c073b7ee4e0b8eec3c3ab70
simonamadeos <shalomamadeo03@gmail.com>
2026-04-23 22:40:50 +0700

    update db connection to use env vars

--- config/db.php
+++ config/db.php
@@ -1,13 +1,14 @@
 <?php
 
+
 return [
     'class' => 'yii\db\Connection',
-    // Gunakan getenv() untuk mengambil data dari Railway
+    // Mengambil data dari environment variable Railway
     'dsn' => 'mysql:host=' . getenv('MYSQLHOST') . ';port=' . getenv('MYSQLPORT') . ';dbname=' . getenv('MYSQLDATABASE'),
     'username' => getenv('MYSQLUSER'),
     'password' => getenv('MYSQLPASSWORD'),
     'charset' => 'utf8mb4',
-
+    
     // Schema cache options (opsional)
     'enableSchemaCache' => true,
     'schemaCacheDuration' => 60,
